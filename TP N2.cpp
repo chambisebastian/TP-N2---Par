5b16eec493ba713137b1f27f78e30b0f990e6cd2
chambisebastian <reydeltenis_6@hotmail.com>
2015-04-23 09:30:03 -0300

    Create TP N2.cpp

---
+++ TP N2.cpp
@@ -0,0 +1,21 @@
+/*
+Descrpcion: Programa que maneja datos de tipo entero, string y booleano
+Autor: Sebastian Matias Chambi
+23/04/2015
+*/
+ 
+#include <iostream>
+
+int main() {
+	int numero;
+	std::cout << "Ingrese un numero entero: ";
+	std::cin >> num;
+	if ((numero%2)==0)
+	{
+		std::cout << "\nEl numero ingresado es par";
+	}
+	else
+	{
+		std::cout << "\nEl numero ingresado es impar";
+	}
+}
